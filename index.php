ab4b65c72443b5b9c50c4fe197c8713df88865b8
Joaquin-1204 <joaquin.salas@tecsup.edu.pe>
2022-11-24 00:45:18 -0500

    foods 4

--- index.php
+++ index.php
@@ -68,63 +68,65 @@ Flight::route('DELETE /users/@id', function ($id) {
 });
 Flight::start();
 
+
+
 //datos de comidas
 //GET
 Flight::route('GET /foods', function () {
-    $sentencia= Flight::db()->prepare("SELECT * FROM `foods`");
-    $sentencia->execute();
-    $datos=$sentencia->fetchAll();
-    Flight::json($datos);
+    $sentencia2= Flight::db()->prepare("SELECT * FROM `foods`");
+    $sentencia2->execute();
+    $datos2=$sentencia2->fetchAll();
+    Flight::json($datos2);
 });
 //GET unitario
 Flight::route('GET /foods/@id', function ($id) {
-    $sentencia= Flight::db()->prepare("SELECT * FROM  `foods` WHERE id=?");
-    $sentencia->bindParam(1,$id);
+    $sentencia2= Flight::db()->prepare("SELECT * FROM  `foods` WHERE id=?");
+    $sentencia2->bindParam(1,$id);
 
-    $sentencia->execute();
-    $datos=$sentencia->FetchAll();
-    Flight::json($datos);
+    $sentencia2->execute();
+    $datos2=$sentencia2->FetchAll();
+    Flight::json($datos2);
 });
 //POST
 Flight::route('POST /foods', function () {
     //recepecion datos
-    $nameco=(Flight::request()->data->nameco);
-    $calorias=(Flight::request()->data->calorias);
-    $imagen=(Flight::request()->data->imagen);
+    $nameco=(Flight::request()->data2->nameco);
+    $calorias=(Flight::request()->data2->calorias);
+    $imagen=(Flight::request()->data2->imagen);
 
     $sql="INSERT INTO foods (nameco,calorias,imagen) VALUES (?,?,?)";
     //agregavalor
-    $sentencia= Flight::db()->prepare($sql);
-    $sentencia->bindParam(1, $nameco);
-    $sentencia->bindParam(2, $calorias);
-    $sentencia->bindParam(3, $imagen);
-    $sentencia->execute();
+    $sentencia2= Flight::db()->prepare($sql);
+    $sentencia2->bindParam(1, $nameco);
+    $sentencia2->bindParam(2, $calorias);
+    $sentencia2->bindParam(3, $imagen);
+    $sentencia2->execute();
     Flight::jsonp(["Comida agregado"]);
 });
 //PUT
 Flight::route('PUT /users/@id', function ($id) {
-    $nameco=(Flight::request()->data->nameco);
-    $calorias=(Flight::request()->data->calorias);
-    $imagen=(Flight::request()->data->imagen);
+    $nameco=(Flight::request()->data2->nameco);
+    $calorias=(Flight::request()->data2->calorias);
+    $imagen=(Flight::request()->data2->imagen);
     //no enviar id
     $sql="UPDATE foods SET nameco=?, calorias=?, imagen=? WHERE id=?";
     //modificavalor
-    $sentencia= Flight::db()->prepare($sql);
-    $sentencia->bindParam(1, $nameco);
-    $sentencia->bindParam(2, $calorias);
-    $sentencia->bindParam(3, $imagen);
-    $sentencia->bindParam(4, $id);
+    $sentencia2= Flight::db()->prepare($sql);
+    $sentencia2->bindParam(1, $nameco);
+    $sentencia2->bindParam(2, $calorias);
+    $sentencia2->bindParam(3, $imagen);
+    $sentencia2->bindParam(4, $id);
 
-    $sentencia->execute();
+    $sentencia2->execute();
     Flight::jsonp(["Comida modificado"]);
 });
 //DELETE
 Flight::route('DELETE /foods/@id', function ($id) {
     $sql="DELETE FROM foods WHERE id=?";
     
-    $sentencia= Flight::edb()->prepare($sql);
-    $sentencia->bindParam(1, $id);
-    $sentencia->execute();
+    $sentencia2= Flight::edb()->prepare($sql);
+    $sentencia2->bindParam(1, $id);
+    $sentencia2->execute();
     Flight::jsonp(["Comida borrado"]);
 });
 Flight::start();
